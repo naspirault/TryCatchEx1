66d5b38943969af2cae2865d26de641b5f4cd37e
Nancy <naspirault@gmail.com>
2023-04-03 09:36:40 -0400

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         x = readInteger(msg,0,tableau.length-1);
 */
-
+/*Allô! */
         while (true){
             System.out.print("Entrez l'indice du tableau: ");
             x = sc.nextInt();
